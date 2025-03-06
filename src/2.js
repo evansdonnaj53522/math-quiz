0716d5e7bedd6efcd74d482881ccdd7bd6789a0c
evansdonnaj53522 <evans_donnaj53522@gmx.com>
2025-03-06 18:26:17 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,56 @@
+let questions = [
+  {
+    question: "What is the sum of 2 + 2?",
+    answers: [
+      {text: "5", correct: false},
+      {text: "4", correct: true},
+      {text: "6", correct: false},
+      {text: "10", correct: false}
+    ]
+  },
+  {
+    question: "What is the product of 3 and 3?",
+    answers: [
+      {text: "9", correct: true},
+      {text: "6", correct: false},
+      {text: "12", correct: false},
+      {text: "18", correct: false}
+    ]
+  },
+  {
+    question: "What is the quotient of 12 divided by 4?",
+    answers: [
+      {text: "3", correct: true},
+      {text: "6", correct: false},
+      {text: "9", correct: false},
+      {text: "12", correct: false}
+    ]
+  },
+  {
+    question: "What is the difference of 8 - 5?",
+    answers: [
+      {text: "3", correct: true},
+      {text: "4", correct: false},
+      {text: "6", correct: false},
+      {text: "10", correct: false}
+    ]
+  },
+  {
+    question: "What is the product of 2 and 5?",
+    answers: [
+      {text: "10", correct: true},
+      {text: "8", correct: false},
+      {text: "12", correct: false},
+      {text: "16", correct: false}
+    ]
+  },
+  {
+    question: "What is the sum of 7 + 3?",
+    answers: [
+      {text: "10", correct: true},
+      {text: "9", correct: false},
+      {text: "12", correct: false},
+      {text: "14", correct: false}
+    ]
+  }
+];
